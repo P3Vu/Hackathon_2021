3e58fc0d28240906b86a5785d5516713c9c2b72e
Mikolaj <mikbarcikowski@gmail.com>
2021-04-10 05:37:44 +0200

    mysql

--- zabezpieczenia/gsm.py
+++ zabezpieczenia/gsm.py
@@ -1,8 +1,35 @@
+#!/usr/bin/env python3
 import time
 import serial
+import mysql.connector
+from mysql.connector import Error
 
 recipient = "+48516187461"
 message = "Hello, World!"
+
+def get_number():
+    try:
+        connection = mysql.connector.connect(host='localhost',
+                                             database='hackathon',
+                                             user='luiza',
+                                             password='haslo')
+        if connection.is_connected():
+            sql_select_Query = "select * from kitchen_fire"
+            if not sql_select_Query:
+                print("This table is empty")
+            #cursor = connection.cursor()
+            #cursor.execute(sql_select_Query)
+            #records = cursor.fetchone()
+
+    except Error as e:
+        print("Error while connecting to MySQL", e)
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("MySQL connection is closed")
+
+
 def send_sms():
     phone = serial.Serial("/dev/serial0",  9600, timeout=5)
     phone.write('ATE0'+'\r')
@@ -16,4 +43,5 @@ def send_sms():
     phone.write("\x1A")
 
 if __name__ == '__main__':
-    send_sms()
+    get_number()
+    #send_sms()
